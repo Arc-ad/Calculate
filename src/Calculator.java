cd4b82947d9ce9d03f044c6b0d2364d896d54fb4
Arc-ad <barkovskiia@bk.ru>
2022-06-14 23:59:02 +0300

    запись ответа в файл

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,35 +1,48 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Calculator {
-    void read(File file) throws FileNotFoundException{
+    private String temp = "";
+    String read(File file) throws FileNotFoundException{     // Метод считывает текст из файла в переменную temp и
+                                                            // и возращает стсроковое значение
         Scanner sc = new Scanner(file);
-        String temp = sc.nextLine();
-        System.out.println(temp);
+        temp = sc.nextLine();
+        return temp;
     }
 
+    void writer(File file, String str) throws IOException {         // метод записывает в файл ответ
+        try (FileWriter writer = new FileWriter(file, false)) {
+            writer.write(str);
+            writer.flush();
+        }
 
+    }
 
-    void calculate(String str){
+    double calculate(String str){
         String[] s = str.split("\\s+");
         try{
             double a = Double.parseDouble(s[0]);
             String st = s[1];
             double b = Double.parseDouble(s[2]);
-            if(st.equals("+")){
-                System.out.println(a + b);
-            }else if (st.equals("-"))
-                System.out.println(a - b);
-            else if (st.equals("*"))
-                System.out.println(a * b);
-            else if (st.equals("/")) {
-                if(b == 0){
-                    throw new Exception("Error! Division by zero");
-                }
-                System.out.println(a / b);
+            switch (st) {
+                case "+":
+                    return a + b;
+                case "-":
+                    return a - b;
+                case "*":
+                    return a * b;
+                case "/":
+                    if (b == 0) {
+                        throw new Exception("Error! Division by zero");
+                    }
+                    return a / b;
+
+                default:
+                    throw new Exception("Operation Error!");
             }
-            else throw new Exception("Operation Error!");
         }catch( NumberFormatException nfe){
             System.out.println("Error! Not number");
 
@@ -37,5 +50,6 @@ public class Calculator {
             System.out.println(ex.getMessage());
         }
 
+        return 0;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
+
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         String path = "bro.txt";
         File file = new File(path);
         Calculator c = new Calculator();
-        c.read(file);
-
-
+       String result = String.valueOf(c.calculate(c.read(file)));
+        File file1 = new File("broOutput.txt");
+        c.writer(file1, result);
     }
 }
